b6531d86be105cdda4ecde57d364c6a0a66cfd30
athasim <38163406+athasim@users.noreply.github.com>
2021-06-29 10:43:14 +0200

    Update MRV_data_analysis.R

--- MRV_data_analysis.R
+++ MRV_data_analysis.R
@@ -2,7 +2,7 @@
 ## "Inclusion of Shipping in the EU-ETS: Assesing the direct costs for the maritime sector using the MRV data".
 ## You are free to use it but please make a reference to the paper.
 ## For any questions about the R code you ar free to contact the author (Athanasios Simoulis) at 
-## at: thanasis @ simoulis.eu. The code author has the full responsibility for this.
+## at: thanasis @ simoulis.eu. The code author has the full responsibility for the code.
 ## For any questions about the results interpretation and the paper please contact:
 ## Dr Anastasia Christodoulou  at: ac @ wmu.se or at: anastasiachristo @ gmail.com
 
