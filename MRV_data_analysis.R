3b0f8abf9bc3422d27a9029c0cf0d3641ac78816
athasim <38163406+athasim@users.noreply.github.com>
2021-06-30 19:02:34 +0200

    MRV_data_analysis.R

--- MRV_data_analysis.R
+++ MRV_data_analysis.R
@@ -1,5 +1,12 @@
 ## Supplementary R file code of the paper: 
 ## "Inclusion of Shipping in the EU-ETS: Assesing the direct costs for the maritime sector using the MRV data".
+## https://doi.org/10.3390/en14133915
+## MDPI and ACS Style
+##Christodoulou, A.; Dalaklis, D.; Ölçer, A.I.; Ghaforian Masodzadeh, P. Inclusion of Shipping in the EU-ETS: Assessing the Direct Costs for the Maritime Sector Using the MRV Data. Energies 2021, 14, 3915. https://doi.org/10.3390/en14133915
+## AMA Style
+##Christodoulou A, Dalaklis D, Ölçer AI, Ghaforian Masodzadeh P. Inclusion of Shipping in the EU-ETS: Assessing the Direct Costs for the Maritime Sector Using the MRV Data. Energies. 2021; 14(13):3915. https://doi.org/10.3390/en14133915
+##Chicago/Turabian Style
+##Christodoulou, Anastasia, Dimitrios Dalaklis, Aykut I. Ölçer, and Peyman Ghaforian Masodzadeh. 2021. "Inclusion of Shipping in the EU-ETS: Assessing the Direct Costs for the Maritime Sector Using the MRV Data" Energies 14, no. 13: 3915. https://doi.org/10.3390/en14133915
 ## You are free to use it but please make a reference to the paper.
 ## For any questions about the R code you ar free to contact the author (Athanasios Simoulis) at 
 ## at: thanasis @ simoulis.eu. The code author has the full responsibility for the code.
